fd6cd39a6401639dfb46273ec32a6eae3b4d6bfe
TheTimeBandit2k <TimeBandit2k@gmail.com>
2026-04-21 18:26:08 -0500

    Added simple reading and update logic

--- src/main/java/com/student/gymdb/dao/MemberDAO.java
+++ src/main/java/com/student/gymdb/dao/MemberDAO.java
@@ -1,18 +1,20 @@
 package com.student.gymdb.dao;
 
 import com.student.gymdb.model.Member;
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class MemberDAO {
-    
+
     public void insertMember(Member member) {
         String query = "INSERT INTO MemberTable (MemberID, FullName, MembershipType, JoinDate, ExpiryDate, Phone, TrainerName) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
-            
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+
             pstmt.setInt(1, member.getMemberID());
             pstmt.setString(2, member.getFullname());
             pstmt.setString(3, member.getMembershipType());
@@ -20,15 +22,82 @@ public class MemberDAO {
             pstmt.setDate(5, member.getExpiryDate());
             pstmt.setString(6, member.getPhone());
             pstmt.setString(7, member.getTrainerName());
-            
+
             pstmt.executeUpdate();
             System.out.println("Insert of " + member.getFullname() + " successful.");
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.err.println("FAILURE: Could not insert!");
             System.err.println(e.getMessage());
         }
-        
+
         //TODO: Add remainder up update and deletion logic
     }
+
+    public List<Member> getPremiumMembersExpiring() {
+        List<Member> members = new ArrayList<>();
+
+        // SQL logic to get the range of memberships expiring in 30 days
+        String query = "SELECT * FROM MemberTable WHERE MembershipType = 'Premium' AND ExpiryDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 30 DAY)";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query); ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                members.add(new Member(
+                        rs.getInt("MemberID"), rs.getString("FullName"), rs.getString("MembershipType"),
+                        rs.getDate("JoinDate"), rs.getDate("ExpiryDate"), rs.getString("Phone"), rs.getString("TrainerName")
+                ));
+            }
+
+        } catch (SQLException e) {
+            System.err.println("FAILURE: Could not fetch expiring members!");
+            System.err.println(e.getMessage());
+        }
+
+        return members;
+    }
+
+    public List<Member> getMembersByTrainer(String trainerName) {
+        List<Member> members = new ArrayList<>();
+        String sql = "SELECT * FROM MemberTable WHERE TrainerName = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, trainerName);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    members.add(new Member(
+                            rs.getInt("MemberID"), rs.getString("FullName"), rs.getString("MembershipType"),
+                            rs.getDate("JoinDate"), rs.getDate("ExpiryDate"), rs.getString("Phone"), rs.getString("TrainerName")
+                    ));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("FAILURE: Could not fetch members by trainer!");
+            System.err.println(e.getMessage());
+        }
+        return members;
+    }
+    
+    public void updateMembershipType(int memberID, String membershipType) {
+        String query = "UPDATE MemberTable SET MembershipType = ? Where MemberID = ?";
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, membershipType);
+            pstmt.setInt(2, memberID);
+            
+            int rowsAffected = pstmt.executeUpdate();
+            
+            if (rowsAffected > 0 ) {
+                System.out.println("SUCCESS: Update complete for member ID " + memberID);
+            }
+            else {
+                System.out.println("No member ID found for " + memberID);
+            }
+        }
+        catch (SQLException  e) {
+            System.err.println("FAILURE: Could not update membership type!");
+            System.out.println(e.getMessage());
+        }
+    }
+    
 }
--- src/main/java/com/student/gymdb/dao/WorkoutLogDAO.java
+++ src/main/java/com/student/gymdb/dao/WorkoutLogDAO.java
@@ -1,9 +1,12 @@
 package com.student.gymdb.dao;
 
 import com.student.gymdb.model.WorkoutLog;
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class WorkoutLogDAO {
     
@@ -30,5 +33,69 @@ public class WorkoutLogDAO {
         }
     }
     
-    //TODO: FInish backend log search and update logic
+    public List<WorkoutLog> getHighCalorieWorkouts() {
+        List<WorkoutLog> logs = new ArrayList<>();
+        String query = "SELECT * FROM WorkoutLogTable where CaloriesBurned > 500";
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query); ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                logs.add(new WorkoutLog(
+                        rs.getInt("LogID"), rs.getInt("MemberID"), rs.getDate("WorkoutDate"), rs.getString("exercisename"),
+                        rs.getInt("DurationMins"), rs.getInt("CaloriesBurned"), rs.getString("Intensity")
+                ));
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("FAILURE: Could not get high-calorie workouts!");
+            System.err.println(e.getMessage());
+        }
+        
+        return logs;
+    }
+    
+    public List<WorkoutLog> getLogbyMemberID(int memberID) {
+        List<WorkoutLog> logs = new ArrayList<>();
+        String query = "SELECT * FROM WorkoutLogTable WHERE MemberID = ?";
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, memberID);
+            
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while(rs.next()) {
+                    logs.add(new WorkoutLog(
+                            rs.getInt("LogID"), rs.getInt("MemberID"), rs.getDate("WorkoutDate"), rs.getString("exercisename"),
+                            rs.getInt("DurationMins"), rs.getInt("CaloriesBurned"), rs.getString("Intensity")
+                    ));
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("FAILURE: could not get log by Member ID!");
+            System.err.println(e.getMessage());
+        }
+        
+        return logs;
+    }
+    
+    public void updateWorkoutDuration(int logID, int duration) {
+        String query = "UPDATE WorkoutLogTable SET DurationMins = ? WHERE LogID = ?";
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, duration);
+            pstmt.setInt(2, logID);
+            
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("SUCCESS: Duration changed for log " + logID);
+            }
+            else {
+                System.out.println("No log entry found for LogID " + logID);
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("FAILURE: Could not update log!");
+            System.err.println(e.getMessage());
+        }
+    }
+    
 }
